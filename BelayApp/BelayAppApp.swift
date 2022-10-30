e44ad24681f70eea9c40bf1fd2de4f5bed70b857
Christopher-Zhang <chrisconcord@gmail.com>
2022-10-30 00:15:14 -0500

    Initial Commit

---
+++ BelayApp/BelayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BelayAppApp.swift
+//  BelayApp
+//
+//  Created by Christopher Zhang on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct BelayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
